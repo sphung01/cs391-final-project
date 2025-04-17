c5833cbe091d01eb6e18a900cb400aa45d0f42a7
Steven Phung <sphung01@bu.edu>
2025-04-17 12:07:48 -0400

    Added enough components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import SongGenerator from "@/components/SongGenerator";
+
 export default function Home() {
   return (
     <div>
       Hello World
+      <SongGenerator />
     </div>
   );
 }
--- components/SongGenerator.tsx
+++ components/SongGenerator.tsx
@@ -3,7 +3,7 @@
 export default function SongGenerator() {
     return(
         <div>
-            
+            This is where the SongGenerator is.
         </div>
     )
 }
