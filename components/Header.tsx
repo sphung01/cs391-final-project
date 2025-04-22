fedbe8acc3f6c04a72ff6a5ea05725427661a42e
Steven Phung <sphung01@bu.edu>
2025-04-22 16:27:26 -0400

    Added a comment

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+// Worked on by Steven Phung
+
 /* 
     In the Header.tsx component, it's basically a full-width bar that
     has the title and the tabs (Like the "about" page) that leads to a different page
