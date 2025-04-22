3755e16d8003b682cff05a8aab502395aad3fa71
Steven Phung <sphung01@bu.edu>
2025-04-22 16:51:17 -0400

    Added code in types.ts Steven Phung

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,10 @@
 /* 
     For extracting the API, up to you on what types you want to get from the data.
 */
+
+export interface SongProps {
+    /* 
+        Here is where you can enter the types when extracting the song.
+        The decision is up to you
+    */
+} 
