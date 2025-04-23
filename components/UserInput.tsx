3d97fc4f602e1680e4c6cd2ce520a3c8ce2ea2e0
Julian Lim <limjunre@bu.edu>
2025-04-23 01:29:44 -0400

    implemented skeleton of UserInput component

--- components/UserInput.tsx
+++ components/UserInput.tsx
@@ -8,14 +8,29 @@
 // Steven Phung: I have formatted where this form wraps the button and input field. Rest is up to you.
 "use client"
 import getSongs from "@/lib/getSongs"
+import {useState} from "react"
+
+
+
+
 export default function UserInput() {
+    const [genre, setGenre] = useState<string>("");
+
+    /*styling*/
     const inputFormStyling = "flex flex-col items-center justify-center w-[50vw] h-[30vh] bg-green-500 rounded-3xl drop-shadow-lg]";
-    const generateButtonStyling = "w-full my-[1vh] bg-green-600 text-white font-bold py-[1vh] px-[2vw] rounded-lg hover:bg-green-800 shadow-lg transition";
+    const generateButtonStyling = "w-full my-[1vh] bg-green-600 text-white font-bold py-[1vh] px-[2vw] rounded-lg shadow-lg active:bg-green-900 hover:bg-green-800 transition";
     const titleStyling = "text-[calc(3px+1.5vw)] font-semibold text-white my-[1vh]";
     const inputStyling = "w-full p-[1vh] rounded-lg border-4 border-white focus:outline-none focus:ring-2"
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(genre);
+        await getSongs(genre);
+    };
+    
     return(
         <div>
-            <form className={inputFormStyling}>
+            <form onSubmit={handleSubmit} className={inputFormStyling}>
                 <h1 className={titleStyling}>Playlist Generator</h1>
                 <div className="flex flex-col my-[1vh] w-[50%]">
                     <label htmlFor="genre" className="">Enter a genre:</label>
@@ -23,9 +38,11 @@ export default function UserInput() {
                         id="genre"
                         type="text"
                         placeholder="e.g. Pop, Rock, Jazz"
+                        value={genre}
+                        onChange={(e) => setGenre(e.target.value)}
                         className={inputStyling}
                     />
-                    <button className={generateButtonStyling}>Generate</button>
+                    <button type="submit" className={generateButtonStyling}>Generate</button>
                 </div>
             </form>
         </div>
