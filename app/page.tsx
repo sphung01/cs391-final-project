1e123bd3d3b75ed85b05dfad87da7747bdc4040a
Julian Lim <limjunre@bu.edu>
2025-04-23 23:50:24 -0400

    updated layout.tsx header and removed redundant imports in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@
 */
 "use client"
 import UserInput from "@/components/UserInput"; // done by Julian Lim Jun Ren
-import { useState } from "react";
-import { Song } from "@/lib/types"; 
 export default function Home() {
   const mainStyling = "min-h-screen w-full flex justify-center items-center";
   return (
