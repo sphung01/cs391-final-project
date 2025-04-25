b2a36d94398635af2019ba7b03b15ff272d8a4c0
Julian Lim Jun Ren <limjunre@bu.edu>
2025-04-25 17:31:13 -0400

    revamped the album list styling as cards instead of a list, with responsive design

--- components/AlbumComponent.tsx
+++ components/AlbumComponent.tsx
@@ -3,7 +3,22 @@
 import {AlbumProps} from "@/lib/types";
 import Link from "next/link";
 
-const AlbumStyling = "flex flex-col gap-2 max-w-3xl min-w-[30vw] w-full p-4 rounded-2xl shadow-lg bg-[#dee2e6] hover:bg-[#e9ecef] hover:shadow-[0px_0px_15px_2px_#e9ecef4d] transition "
+const albumContainerStyling = "flex flex-col gap-2 max-w-3xl min-w-[30vw] w-full p-4 rounded-2xl shadow-lg bg-[#dee2e6] hover:bg-[#e9ecef] hover:shadow-[0px_0px_15px_2px_#e9ecef4d] transition "
+const subtitleStyling = "text-sm text-gray-500 ml-10"
+const albumNameStyling = "text-lg font-semibold text-black"
+const artistNameStyling = "text-sm text-gray-500"
+const albumCoverStyling = "w-20 h-20 rounded shadow-lg"
+const albumImgTitleContainerStyling = "flex items-center gap-4"
+const albumTitleContainerStyling = "flex flex-col gap-4"
+
+const altAlbumContainerStyling = "flex flex-col w-[40vw] sm:w-[20vw] p-4 rounded-2xl shadow-lg bg-[#e9ecef] hover:bg-[#dee2e6] hover:shadow-[0px_0px_15px_2px_#dee2e6] hover:scale-102 transition "
+const altAlbumCoverStyling = "w-full rounded drop-shadow-lg"
+const altAlbumNameStyling = "text-lg font-bold text-black truncate tracking-wide";
+const altArtistNameStyling = "text-sm italic text-gray-600 truncate";
+const altAlbumSubtitleStyling = "text-xs text-gray-500 text-center truncate"
+const altAlbumImgTitleContainerStyling = "flex flex-col items-center w-full"
+const altAlbumTitleContainerStyling = "flex flex-col w-full my-[1vh] text-center"
+
 
 function ArtistTitle(title: string): {artist: string, albumname: string} {
     const seperator = title.indexOf("-");   //split on first -
@@ -23,19 +38,19 @@ export default function AlbumComponent({ album }: AlbumProps) {
     return (
         <Link
             href={`/album/${album.id}`}
-            className={AlbumStyling}
+            className={altAlbumContainerStyling}
         >
             {/* img + title container */}
             <div
-                className="flex items-center gap-4"
+                className={altAlbumImgTitleContainerStyling}
             >
-                <img src={album.cover_image} alt={`cover of ${album.title}`} className="w-20 h-20 rounded shadow-lg" />
-                <div className="flex flex-col gap-4">
-                    <h2 className="text-lg font-semibold text-black">{albumname}</h2>
-                    <p className="text-sm text-gray-500">{artist}</p>
+                <img src={album.cover_image} alt={`cover of ${album.title}`} className={altAlbumCoverStyling} />
+                <div className={altAlbumTitleContainerStyling}>
+                    <h2 className={altAlbumNameStyling}>{albumname}</h2>
+                    <p className={altArtistNameStyling}>{artist}</p>
                 </div>
             </div>
-            <p className="text-sm text-gray-500 ml-10">
+            <p className={altAlbumSubtitleStyling}>
                 {
                     [album.year, album.country, ...album.genre]
                         .filter(e => e) //remove empty strings
--- components/AlbumList.tsx
+++ components/AlbumList.tsx
@@ -3,11 +3,14 @@
 import AlbumComponent from "./AlbumComponent";
 import { Album } from "@/lib/types";
 
+const albumListStyling = "flex flex-col gap9";
+const altAlbumListStyling = "flex flex-row flex-wrap justify-center gap-[2vw]";
+
 export default function AlbumList({albums}: {albums: Album[]}) {
     console.log(albums);
 
     return (
-        <div className="flex flex-col gap-9">
+        <div className={altAlbumListStyling}>
             {
                 albums.map((album, index) => (
                     <AlbumComponent key={index} album={album} />
